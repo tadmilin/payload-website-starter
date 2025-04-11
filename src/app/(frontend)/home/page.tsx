e621c9c0e223a5b935994cf228210b1505aeb677
tadmilin <tadeyes1@gmail.com>
2025-04-11 11:52:30 +0700

    เปลี่ยนการ import Navbar เป็น relative path

--- src/app/(frontend)/home/page.tsx
+++ src/app/(frontend)/home/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Navbar } from '@/components/Navbar'
+import { Navbar } from '../../../components/Navbar'
 
 export default function HomePage() {
   const [currentSlide, setCurrentSlide] = useState(0);
